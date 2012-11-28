fb9028756e60686dc42469ccab5c389d23e07d79
Martijn van de Rijdt <mvdrijdt@planet.nl>
2012-11-28 16:40:22 +0100

    Added some logging, caught any IOExceptions and RunTimeExceptions.

--- umvc3replayanalyser/src/main/java/nl/tinus/umvc3replayanalyser/gui/Umvc3ReplayManager.java
+++ umvc3replayanalyser/src/main/java/nl/tinus/umvc3replayanalyser/gui/Umvc3ReplayManager.java
@@ -1,19 +1,19 @@
 package nl.tinus.umvc3replayanalyser.gui;
 
-import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Main class, used to start the application. Defines the JavaFX user interface.
  * 
  * @author Martijn van de Rijdt
  */
+@Slf4j
 public class Umvc3ReplayManager extends Application {
     /**
      * Main method, starts the application.
@@ -27,12 +27,24 @@ public class Umvc3ReplayManager extends Application {
 
     /** {@inheritDoc} */
     @Override
-    public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/ui.fxml"));
-        stage.setTitle("Ultimate Marvel vs Capcom 3 Replay Manager");
-        stage.setScene(new Scene(root));
-        // TODO resize this icon, maybe get the UMvC3 player icons?
-        stage.getIcons().add(new Image("ultimate-marvel-vs-capcom-3.jpg"));
-        stage.show();
+    public void start(Stage stage) {
+        try {
+            log.info("Starting application.");
+            Parent root = FXMLLoader.load(getClass().getResource("/ui.fxml"));
+
+            log.info("Fxml loaded, performing additional initialisation.");
+            stage.setTitle("Ultimate Marvel vs Capcom 3 Replay Manager");
+            stage.setScene(new Scene(root));
+            // TODO resize this icon, maybe get the UMvC3 player icons?
+            stage.getIcons().add(new Image("ultimate-marvel-vs-capcom-3.jpg"));
+
+            log.info("Showing UI.");
+            stage.show();
+
+            log.info("Application started.");
+        } catch (Exception e) {
+            log.error("Unable to start application.", e);
+            // TODO show a simple error message dialog to the user, which does not require fxml
+        }
     }
 }
