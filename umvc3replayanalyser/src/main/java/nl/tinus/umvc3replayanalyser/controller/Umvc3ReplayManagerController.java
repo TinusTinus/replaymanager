ab47d22d91b6fb76d76bcb6ea697f3e8dbc5593b
Martijn van de Rijdt <mvdrijdt@planet.nl>
2012-11-28 20:22:52 +0100

    Added a controller which performs some initialisation.

---
+++ umvc3replayanalyser/src/main/java/nl/tinus/umvc3replayanalyser/controller/Umvc3ReplayManagerController.java
@@ -0,0 +1,44 @@
+package nl.tinus.umvc3replayanalyser.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import lombok.extern.slf4j.Slf4j;
+import nl.tinus.umvc3replayanalyser.model.Game;
+
+/**
+ * Main controller class for this JavaFX application. Referenced from the corresponding fxml.
+ * 
+ * @author Martijn van de Rijdt
+ */
+@Slf4j
+public class Umvc3ReplayManagerController {
+    /** Preview image view. */
+    @FXML
+    private ImageView previewImageView;
+    /** Anchor pane containing the preview image view. */
+    @FXML
+    private AnchorPane previewImageAnchorPane;
+    /** The default preview image. Whenever no replay is selected, this one should be displayed. */
+    @FXML
+    private Image defaultPreviewImage;
+    /** The main table view. */
+    @FXML
+    private TableView<Game> replayTableView;
+
+    /** Initialisation method. */
+    @FXML
+    public void initialize() {
+        log.info("Performing controller initialisation.");
+
+        // Bind the image size to the size of its parent.
+        previewImageView.fitWidthProperty().bind(previewImageAnchorPane.widthProperty());
+        previewImageView.fitHeightProperty().bind(previewImageAnchorPane.heightProperty());
+        
+        // TODO tableView initialisation
+
+        log.info("Initialisation complete.");
+    }
+}
---
+++ umvc3replayanalyser/src/main/java/nl/tinus/umvc3replayanalyser/controller/package-info.java
@@ -0,0 +1,2 @@
+/** Contains controllers for the user interface components. */
+package nl.tinus.umvc3replayanalyser.controller;
