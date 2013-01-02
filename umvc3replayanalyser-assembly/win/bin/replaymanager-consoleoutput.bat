dd4629ff52dfb092cd0227c071cbe6cf1ff0d5dd
Martijn van de Rijdt <mvdrijdt@planet.nl>
2013-01-02 16:54:09 +0100

    Added a second batch file which keeps the console open, so we can follow along with logging or any startup exceptions.

---
+++ umvc3replayanalyser-assembly/win/bin/replaymanager-consoleoutput.bat
@@ -0,0 +1,16 @@
+@echo off
+
+:: Startup script for the Ultimate Marvel vs Capcom 3 Replay Manager.
+:: This script assumes that a valid javaw.exe, version 1.7 or later, is on the PATH.
+
+set PWD="%CD%"
+
+set LIB_DIR=%PWD%/../lib
+
+set CONFIG_DIR=%PWD%/../etc
+
+set JAVA_OPTS=
+
+set JAVA=java
+
+start %JAVA% %JAVA_OPTS% -cp %CONFIG_DIR%;%LIB_DIR%/* nl.tinus.umvc3replayanalyser.gui.Umvc3ReplayManager
