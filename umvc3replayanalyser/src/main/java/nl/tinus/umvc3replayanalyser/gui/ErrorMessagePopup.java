7ab98567d59713e7e94249f3116b1c40b8ae6eae
Martijn van de Rijdt <mvdrijdt@planet.nl>
2013-02-10 13:52:31 +0100

    Extracted the error message popup into a separate class.

---
+++ umvc3replayanalyser/src/main/java/nl/tinus/umvc3replayanalyser/gui/ErrorMessagePopup.java
@@ -0,0 +1,124 @@
+package nl.tinus.umvc3replayanalyser.gui;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+/**
+ * Class used to load and show an error message popup.
+ * 
+ * Does not use FXML to minimise the chances of failure.
+ * 
+ * This is a utility class that cannot be instantiated; use the static method.
+ * 
+ * @author Martijn van de Rijdt
+ */
+// private constructor to prevent utility class instantiation
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@Slf4j
+public class ErrorMessagePopup {
+    /**
+     * Handles an exception that caused program startup to fail, by showing an error message to the user.
+     * 
+     * @param title
+     *            title for the dialog
+     * @param errorMessage
+     *            error message
+     * @param stage
+     *            stage in which to show the error message
+     * @param exception
+     *            exception that caused the error
+     */
+    public static void show(String title, String errorMessage, Stage stage, Exception exception) {
+        log.info("Showing error message dialog to indicate that startup failed.");
+
+        // Create the error dialog programatically without relying on FXML, to minimize the chances of further failure.
+
+        stage.setTitle(title);
+
+        // Error message text.
+        Text text = new Text(errorMessage);
+
+        // Text area containing the stack trace. Not visible by default.
+        final TextArea stackTraceArea = new TextArea(ExceptionUtils.getStackTrace(exception));
+        stackTraceArea.setEditable(false);
+        stackTraceArea.setVisible(false);
+
+        // Details button for displaying the stack trace.
+        Button detailsButton = new Button();
+        detailsButton.setText("Details");
+        detailsButton.setOnAction(new EventHandler<ActionEvent>() {
+            /** {@inheritDoc} */
+            @Override
+            public void handle(ActionEvent event) {
+                log.info("User clicked Details.");
+                stackTraceArea.setVisible(!stackTraceArea.isVisible());
+            }
+        });
+
+        // OK button for closing the dialog.
+        Button okButton = new Button();
+        okButton.setText("OK");
+        okButton.setOnAction(new EventHandler<ActionEvent>() {
+            /** {@inheritDoc} */
+            @Override
+            public void handle(ActionEvent event) {
+                log.info("User clicked OK, exiting the program.");
+                Platform.exit();
+            }
+        });
+
+        // Horizontal box containing the buttons, to make sure they are always centered.
+        HBox buttonsBox = new HBox(5);
+        buttonsBox.getChildren().add(detailsButton);
+        buttonsBox.getChildren().add(okButton);
+        buttonsBox.setAlignment(Pos.CENTER);
+
+        // Layout constraints.
+        AnchorPane.setTopAnchor(text, Double.valueOf(5));
+        AnchorPane.setLeftAnchor(text, Double.valueOf(5));
+        AnchorPane.setRightAnchor(text, Double.valueOf(5));
+
+        AnchorPane.setTopAnchor(stackTraceArea, Double.valueOf(31));
+        AnchorPane.setLeftAnchor(stackTraceArea, Double.valueOf(5));
+        AnchorPane.setRightAnchor(stackTraceArea, Double.valueOf(5));
+        AnchorPane.setBottomAnchor(stackTraceArea, Double.valueOf(36));
+
+        AnchorPane.setLeftAnchor(buttonsBox, Double.valueOf(5));
+        AnchorPane.setRightAnchor(buttonsBox, Double.valueOf(5));
+        AnchorPane.setBottomAnchor(buttonsBox, Double.valueOf(5));
+
+        AnchorPane root = new AnchorPane();
+        root.getChildren().addAll(text, stackTraceArea, buttonsBox);
+
+        stage.setScene(new Scene(root));
+
+        // Use a standard program icon if possible.
+        try {
+            stage.getIcons().add(Icons.get().getRandomPortrait());
+        } catch (IllegalStateException e) {
+            log.warn("Failed to load icon for error dialog; proceeding with default JavaFX icon.", e);
+        }
+
+        stage.show();
+
+        // Default size should also be the minimum size.
+        stage.setMinWidth(stage.getWidth());
+        stage.setMinHeight(stage.getHeight());
+
+        log.info("Error dialog displayed.");
+    }
+}
--- umvc3replayanalyser/src/main/java/nl/tinus/umvc3replayanalyser/gui/Umvc3ReplayManager.java
+++ umvc3replayanalyser/src/main/java/nl/tinus/umvc3replayanalyser/gui/Umvc3ReplayManager.java
@@ -1,23 +1,11 @@
 package nl.tinus.umvc3replayanalyser.gui;
 
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.image.Image;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
-
-import org.apache.commons.lang3.exception.ExceptionUtils;
 
 import com.sun.javafx.runtime.VersionInfo;
 
@@ -46,14 +34,14 @@ public class Umvc3ReplayManager extends Application {
     public void start(Stage stage) {
         try {
             logVersionInfo();
-            
+
             log.info("Starting application.");
             Parent root = FXMLLoader.load(getClass().getResource("/umvc3-replay-manager.fxml"));
 
             log.info("Fxml loaded, performing additional initialisation.");
             stage.setTitle(TITLE);
             stage.setScene(new Scene(root));
-            stage.getIcons().add(getIcon());
+            stage.getIcons().add(Icons.get().getRandomPortrait());
 
             log.info("Showing UI.");
             stage.show();
@@ -65,7 +53,8 @@ public class Umvc3ReplayManager extends Application {
             log.info("Application started.");
         } catch (Exception e) {
             log.error("Unable to start application.", e);
-            handleExceptionOnStartup(stage, e);
+            ErrorMessagePopup.show(TITLE + " - failed to start up", TITLE
+                    + " was unable to start due to an unexpected problem.", stage, e);
         }
     }
 
@@ -91,103 +80,5 @@ public class Umvc3ReplayManager extends Application {
     @Override
     public void stop() {
         log.info("Stopping application.");
-    }
-
-    /**
-     * Handles an exception that caused program startup to fail, by showing an error message to the user.
-     * 
-     * @param stage
-     *            stage
-     * @param exception
-     *            exception that caused startup to fail
-     */
-    private void handleExceptionOnStartup(Stage stage, Exception exception) {
-        log.info("Showing error message dialog to indicate that startup failed.");
-
-        // Create the error dialog programatically without relying on FXML, to minimize the chances of further failure.
-
-        stage.setTitle(TITLE + " - failed to start up");
-
-        // Error message text.
-        Text text = new Text(TITLE + " was unable to start due to an unexpected problem.");
-
-        // Text area containing the stack trace. Not visible by default.
-        final TextArea stackTraceArea = new TextArea(ExceptionUtils.getStackTrace(exception));
-        stackTraceArea.setEditable(false);
-        stackTraceArea.setVisible(false);
-
-        // Details button for displaying the stack trace.
-        Button detailsButton = new Button();
-        detailsButton.setText("Details");
-        detailsButton.setOnAction(new EventHandler<ActionEvent>() {
-            /** {@inheritDoc} */
-            @Override
-            public void handle(ActionEvent event) {
-                log.info("User clicked Details.");
-                stackTraceArea.setVisible(!stackTraceArea.isVisible());
-            }
-        });
-
-        // OK button for closing the dialog.
-        Button okButton = new Button();
-        okButton.setText("OK");
-        okButton.setOnAction(new EventHandler<ActionEvent>() {
-            /** {@inheritDoc} */
-            @Override
-            public void handle(ActionEvent event) {
-                log.info("User clicked OK, exiting the program.");
-                Platform.exit();
-            }
-        });
-
-        // Horizontal box containing the buttons, to make sure they are always centered.
-        HBox buttonsBox = new HBox(5);
-        buttonsBox.getChildren().add(detailsButton);
-        buttonsBox.getChildren().add(okButton);
-        buttonsBox.setAlignment(Pos.CENTER);
-
-        // Layout constraints.
-        AnchorPane.setTopAnchor(text, Double.valueOf(5));
-        AnchorPane.setLeftAnchor(text, Double.valueOf(5));
-        AnchorPane.setRightAnchor(text, Double.valueOf(5));
-
-        AnchorPane.setTopAnchor(stackTraceArea, Double.valueOf(31));
-        AnchorPane.setLeftAnchor(stackTraceArea, Double.valueOf(5));
-        AnchorPane.setRightAnchor(stackTraceArea, Double.valueOf(5));
-        AnchorPane.setBottomAnchor(stackTraceArea, Double.valueOf(36));
-
-        AnchorPane.setLeftAnchor(buttonsBox, Double.valueOf(5));
-        AnchorPane.setRightAnchor(buttonsBox, Double.valueOf(5));
-        AnchorPane.setBottomAnchor(buttonsBox, Double.valueOf(5));
-
-        AnchorPane root = new AnchorPane();
-        root.getChildren().addAll(text, stackTraceArea, buttonsBox);
-
-        stage.setScene(new Scene(root));
-
-        // Use a standard program icon if possible.
-        try {
-            stage.getIcons().add(getIcon());
-        } catch (IllegalStateException e) {
-            log.warn("Failed to load icon for error dialog; proceeding with default JavaFX icon.", e);
-        }
-
-        stage.show();
-
-        // Default size should also be the minimum size.
-        stage.setMinWidth(stage.getWidth());
-        stage.setMinHeight(stage.getHeight());
-
-        log.info("Error dialog displayed.");
-    }
-
-    /**
-     * Randomly selects an icon for this instance of the application.
-     * 
-     * @return icon
-     */
-    private Image getIcon() {
-        // Select a random character and use that character's portrait as the icon.
-        return Icons.get().getRandomPortrait();
     }
 }
