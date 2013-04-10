1edb62fd136cd91bcdb9b7bd74a484f10f07ce9b
Martijn van de Rijdt <mvdrijdt@planet.nl>
2013-04-10 21:53:19 +0200

    More cleanup.

--- umvc3replayanalyser/src/main/java/nl/tinus/umvc3replayanalyser/controller/AboutPopupController.java
+++ umvc3replayanalyser/src/main/java/nl/tinus/umvc3replayanalyser/controller/AboutPopupController.java
@@ -32,16 +32,16 @@ class AboutPopupController {
             URIS.put("JavaFX", new URI("http://docs.oracle.com/javafx/"));
             URIS.put("Xuggler", new URI("http://www.xuggle.com/xuggler"));
             // TODO add the following as well
-            // <Hyperlink layoutX="14.0" layoutY="109.0" text="Slf4j" onAction="#handleHyperlinkAction" />
-            // <Hyperlink layoutX="14.0" layoutY="131.0" text="Log4j" onAction="#handleHyperlinkAction" />
-            // <Hyperlink layoutX="14.0" layoutY="153.0" text="Commons-CLI" onAction="#handleHyperlinkAction" />
-            // <Hyperlink layoutX="14.0" layoutY="175.0" text="Commons-lang" onAction="#handleHyperlinkAction" />
-            // <Hyperlink layoutX="14.0" layoutY="197.0" text="Guava" onAction="#handleHyperlinkAction" />
-            // <Hyperlink layoutX="15.0" layoutY="219.0" text="Jackson" onAction="#handleHyperlinkAction" />
-            // <Hyperlink layoutX="103.0" layoutY="65.0" text="BCL for Java SE" onAction="#handleHyperlinkAction" />
-            // <Hyperlink layoutX="104.0" layoutY="87.0" text="GPL v3" onAction="#handleHyperlinkAction" />
-            // <Hyperlink layoutX="104.0" layoutY="109.0" text="MIT" onAction="#handleHyperlinkAction" />
-            // <Hyperlink layoutX="103.0" layoutY="131.0" text="Apache License v2.0" onAction="#handleHyperlinkAction"
+            // "Slf4j"
+            // "Log4j"
+            // "Commons-CLI"
+            // "Commons-lang"
+            // "Guava"
+            // "Jackson"
+            // "BCL for Java SE"
+            // "GPL v3"
+            // "MIT"
+            // "Apache License v2.0"
         } catch (URISyntaxException e) {
             throw new RuntimeException("Unable to complete class initialisation.", e);
         }
