47d16f5c806a5b162537e08d0ac91b0fb878dfdb
Martijn van de Rijdt <mvdrijdt@planet.nl>
2013-08-07 23:38:10 +0200

    Removed unused import.

--- umvc3replayanalyser/src/main/java/nl/mvdr/umvc3replayanalyser/controller/Umvc3ReplayManagerController.java
+++ umvc3replayanalyser/src/main/java/nl/mvdr/umvc3replayanalyser/controller/Umvc3ReplayManagerController.java
@@ -31,7 +31,6 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
