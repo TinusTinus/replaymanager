5116d70ff2e25ccd5b45a08d18a23da728e36d1c
Martijn van de Rijdt <mvdrijdt@planet.nl>
2013-08-07 19:58:01 +0200

    Added a bunch of test cases.

---
+++ umvc3replayanalyser/src/test/java/nl/mvdr/umvc3replayanalyser/controller/ImportReplayTaskTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2013 Martijn van de Rijdt
+ * 
+ * This file is part of the Ultimate Marvel vs Capcom 3 Replay Manager.
+ * 
+ * The Ultimate Marvel vs Capcom 3 Replay Manager is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * The Ultimate Marvel vs Capcom 3 Replay Manager is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with the Ultimate Marvel vs Capcom 3
+ * Replay Manager. If not, see <http://www.gnu.org/licenses/>.
+ */
+package nl.mvdr.umvc3replayanalyser.controller;
+
+import java.io.File;
+import java.util.Collections;
+
+import nl.mvdr.umvc3replayanalyser.config.PropertiesConfiguration;
+import nl.mvdr.umvc3replayanalyser.model.Replay;
+import nl.mvdr.umvc3replayanalyser.video.ReplayAnalyserDummy;
+
+import org.junit.Test;
+
+/**
+ * Test class for {@link ImportReplayTask}.
+ * 
+ * @author Martijn van de Rijdt
+ */
+public class ImportReplayTaskTest {
+
+    /** Tests the constructor. */
+    @Test
+    public void testConstructor() {
+        new ImportReplayTask(new File("."), Collections.<Replay> emptyList(), new ReplayAnalyserDummy(),
+                new ReplaySaver(new PropertiesConfiguration()));
+    }
+
+    /** Tests the constructor. */
+    @Test(expected = NullPointerException.class)
+    public void testConstructorNullDirectory() {
+        new ImportReplayTask(null, Collections.<Replay> emptyList(), new ReplayAnalyserDummy(), 
+                new ReplaySaver(new PropertiesConfiguration()));
+    }
+
+    /** Tests the constructor. */
+    @Test(expected = NullPointerException.class)
+    public void testConstructorNullReplays() {
+        new ImportReplayTask(new File("."), null, new ReplayAnalyserDummy(), 
+                new ReplaySaver(new PropertiesConfiguration()));
+    }
+
+    /** Tests the constructor. */
+    @Test(expected = NullPointerException.class)
+    public void testConstructorNullAnalyser() {
+        new ImportReplayTask(new File("."), Collections.<Replay> emptyList(), null, 
+                new ReplaySaver(new PropertiesConfiguration()));
+    }
+
+    /** Tests the constructor. */
+    @Test(expected = NullPointerException.class)
+    public void testConstructorNullReplaySaver() {
+        new ImportReplayTask(new File("."), Collections.<Replay> emptyList(), new ReplayAnalyserDummy(), null);
+    }
+
+    /** Tests the constructor. */
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorNotADirectory() {
+        new ImportReplayTask(new File("derp"), Collections.<Replay> emptyList(), new ReplayAnalyserDummy(),
+                new ReplaySaver(new PropertiesConfiguration()));
+    }
+}
